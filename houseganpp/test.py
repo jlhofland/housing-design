3059937baff3b1c21dade88d093303abba96defc
Evalexii <a.h.ledbetter@student.tudelft.nl>
2023-10-05 12:59:27 +0200

    saving

--- houseganpp/test.py
+++ houseganpp/test.py
@@ -82,13 +82,13 @@ def main():
         print("getting here")
         # draw real graph and groundtruth
         mks, nds, eds, _, _, eds_f = sample
-        real_nodes = np.where(nds[:,:-2].detach().cpu()==1)[-1]
+        room_types = np.where(nds[:,:-2].detach().cpu()==1)[-1]
         graph = [nds, eds, eds_f]
-        true_graph_obj, graph_im = draw_graph([real_nodes, eds.detach().cpu().numpy()])
+        true_graph_obj, graph_im = draw_graph([room_types, eds.detach().cpu().numpy()])
         graph_im.save('./{}/graph_{}.png'.format(opt.out, i)) # save graph
 
         # add room types incrementally
-        _types = sorted(list(set(real_nodes)))
+        _types = sorted(list(set(room_types)))
         selected_types = [_types[:k+1] for k in range(10)]
         os.makedirs('./{}/'.format(opt.out), exist_ok=True)
         _round = 0
@@ -96,19 +96,19 @@ def main():
         # initialize layout
         state = {'masks': None, 'fixed_nodes': []}
         masks = _infer(graph, model, state)
-        im0 = draw_masks(masks.copy(), real_nodes)
+        im0 = draw_masks(masks.copy(), room_types)
         im0 = torch.tensor(np.array(im0).transpose((2, 0, 1)))/255.0 
         # save_image(im0, './{}/fp_init_{}.png'.format(opt.out, i), nrow=1, normalize=False) # visualize init image
 
         # generate per room type
         for _iter, _types in enumerate(selected_types):
-            _fixed_nds = np.concatenate([np.where(real_nodes == _t)[0] for _t in _types]) \
+            _fixed_nds = np.concatenate([np.where(room_types == _t)[0] for _t in _types]) \
                 if len(_types) > 0 else np.array([]) 
             state = {'masks': masks, 'fixed_nodes': _fixed_nds}
             masks = _infer(graph, model, state)
             
         # save final floorplans
-        imk = draw_masks(masks.copy(), real_nodes)
+        imk = draw_masks(masks.copy(), room_types)
         imk = torch.tensor(np.array(imk).transpose((2, 0, 1)))/255.0 
         save_image(imk, './{}/fp_final_{}.png'.format(opt.out, i), nrow=1, normalize=False)
         
