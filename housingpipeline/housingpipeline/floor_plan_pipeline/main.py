5a5471fb62cf62389e9cfe120145d644f686178d
Jeroen Hofland <hofland.jeroen@gmail.com>
2023-10-20 17:11:55 +0200

    Added UI for houseGan

--- housingpipeline/housingpipeline/floor_plan_pipeline/main.py
+++ housingpipeline/housingpipeline/floor_plan_pipeline/main.py
@@ -5,6 +5,7 @@ import argparse
 import dgl
 import os
 import pickle
+from PIL import Image 
 
 from housingpipeline.floor_plan_pipeline.input_to_graph import create_graph_from_user_input
 from housingpipeline.floor_plan_pipeline.graph_to_floorplan import create_floorplan_from_graph
@@ -20,7 +21,32 @@ def main(args):
     print(g)
     with open("/home/evalexii/Documents/IAAIP/housing-design/housingpipeline/housingpipeline/floor_plan_pipeline/misc/sample_graph_list.p", "rb") as file:
         sample_graph_list = pickle.load(file)
-    create_floorplan_from_graph(graph=sample_graph_list, model_path=args["hhgpp_path"], output_path=args["output_path"])
+
+    # Make a copy of the graph list
+    floorplan_ok = False
+    total_graph_copy = sample_graph_list.copy()
+
+    # Loop till user is satisfied with floorplan
+    while not floorplan_ok:
+        # Create plan
+        create_floorplan_from_graph(graph=sample_graph_list, model_path=args["hhgpp_path"], output_path=args["output_path"])
+
+        # Open image
+        plan = Image.open(args["output_path"] + "/final_pipeline_floorplan.png")
+        plan.show()
+
+        # Check if user is satisfied with floorplan
+        response = input("Is this floorplan ok? (yes/no): ").strip().lower()
+        if response == "yes":
+            floorplan_ok = True
+            print("Amazing your floorplan is ready and saved in the output folder.")
+        elif response == "no":
+            plan.close()
+            sample_graph_list = total_graph_copy.copy()
+            print("Alright generating a new floorplan.")
+        else:
+            print("Invalid input. Please enter either 'yes' or 'no'.")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Full Pipeline")
